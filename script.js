671452cf724c617e69d96f124130a5774746d47f
FlorianZunke <florianzunke@gmx.de>
2024-08-02 20:48:29 +0200

    added filter and design

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-const BASE_URL = "https://pokeapi.co/api/v2/";
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon?limit=200&offset=0";
+const EVOLVES_URL = "https://pokeapi.co/api/v2/evolution-chain/"
+
 let allPokemons = [];
+let currentOffset = 0;
+let pokemonPerPage = 25;
 
+let filtertPokemon = [];
+let pokemonEvolves = [];
 
-function init(params) {
-    fetchPokemon(path = "pokemon?limit=10&offset=0")
-    fetchPokemonStats(path = "pokemon/")
+const STAT_COLORS = {
+    "hp": "rgb(255,224,230)",
+    "attack": "rgb(255,236,217)",
+    "defense": "rgb(234,224,200)",
+    "spz-attack": "rgb(219,242,242)",
+    "spz-defense": "rgb(215,236,251)",
+    "speed": "rgb(235,224,255)",
 }
 
-async function fetchPokemon(path = "pokemon?limit=10&offset=0") {
-    let response = await fetch(BASE_URL + path);
+
+function init() {
+    loadAndShowPkm();
+}
+
+
+async function fetchPokemon() {
+    let response = await fetch(BASE_URL);
     let responseToJson = await response.json();
     allPokemons = await Promise.all(responseToJson.results.map(async (result, index) => {
         const pokemonData = await fetchPokemonData(result.url);
@@ -22,8 +38,6 @@ async function fetchPokemon(path = "pokemon?limit=10&offset=0") {
             stats: pokemonData.stats.map(stat => stat.base_stat),
         }
     }))
-    renderPokemonCard();
-    console.log(allPokemons);
 }
 
 
@@ -34,29 +48,35 @@ async function fetchPokemonData(url) {
 }
 
 
-async function fetchPokemonStats(path = "pokemon/") {
-    let response = await fetch(BASE_URL + path);
-    let responseToJson = await response.json();
-    pokemonStats = await Promise.all(responseToJson.results.map(async (result) => {
-        const pokemonStatsData = await fetchPokemonData(result.url);
-        return {
-            stats: pokemonStatsData.stats.map(stat => stat.base_stat),
-        }
-    }))
-
+async function fetchPokemonEvolutions(evoUrl) {
+    for (let i = 0; i < 3; i++) {
+        let url = evoUrl;
+        let response = await fetch(url);
+        let responseToJson = await response.json();
+        console.log(responseToJson);
+    }
 }
 
 
 function renderPokemonCard() {
     let pokemonCard = document.getElementById('pokemon_card');
-    pokemonCard.innerHTML = '';
 
-    for (let i = 0; i < allPokemons.length; i++) {
-        let firstType = allPokemons[i].type[0];
-        let weightInKg = (allPokemons[i].weight * 0.1).toFixed(1);
+    for (let pokemon = currentOffset; pokemon < currentOffset + pokemonPerPage && pokemon < allPokemons.length; pokemon++) {
+        let firstType = allPokemons[pokemon].type[0];
+        let weightInKg = (allPokemons[pokemon].weight * 0.1).toFixed(1);
 
         weightInKg = weightInKg.endsWith('.0') ? weightInKg.slice(0, -2) : weightInKg;
-        pokemonCard.innerHTML += getAllPokemonHTML(i, firstType, weightInKg);
+        pokemonCard.innerHTML += getAllPokemonHTML(pokemon, firstType, weightInKg);
+    }
+}
+
+
+function loadMorePkm() {
+    currentOffset += pokemonPerPage;
+    if (currentOffset < allPokemons.length) {
+        renderPokemonCard();
+    } else {
+        document.getElementById('load_btn').disabled = true;
     }
 }
 
@@ -72,21 +92,25 @@ function renderBigPokemonCard(pokemon) {
 }
 
 
-function renderPokemonStatsToBigCard(i) {
-    let pokemonStatsContainer = document.getElementById('pokemon_stats');
-    let hp = allPokemons[i].stats[0];
-    let attack = allPokemons[i].stats[1];
-    let defense = allPokemons[i].stats[2];
-    let spezial_attack = allPokemons[i].stats[3];
-    let spezial_defense = allPokemons[i].stats[4];
-    let speed = allPokemons[i].stats[5];
+// function renderPokemonStatsToBigCard(i) {
+//     let pokemonStatsContainer = document.getElementById('pokemon_stats');
 
-    pokemonStatsContainer.innerHTML = '';
-    pokemonStatsContainer.innerHTML += getPokemonStatsHTML(hp, attack, defense, spezial_attack, spezial_defense, speed);
-}
+//     pokemonStatsContainer.innerHTML = '';
+//     pokemonStatsContainer.innerHTML += getPokemonStatsHTML(i);
+// }
+
+
+// function renderPokemonEvolutionsToBigCard(evoUrl, pokemonId) {
+//     let pokemonEvoContainer = document.getElementById('pokemon_evolutions');
+
+//     pokemonEvoContainer.innerHTML = '';
+//     pokemonEvoContainer.innerHTML += getPokemonEvolutionsHTML(evoUrl, pokemonId);
+// pokemon
 
 
 function openBigPokemonCard(pokemon) {
+    let pokemonId = allPokemons[pokemon].id;
+    let evoUrl = `${EVOLVES_URL}${pokemonId}`;
     let overflowContainer = document.getElementById(`overflow_container`);
     let cardRef = document.getElementById(`big_pokemon_card`);
 
@@ -95,7 +119,9 @@ function openBigPokemonCard(pokemon) {
     cardRef.classList.remove('d-none');
 
     renderBigPokemonCard(pokemon);
-    renderPokemonStatsToBigCard(pokemon);
+    // renderPokemonStatsToBigCard(pokemon);
+    fetchPokemonEvolutions(evoUrl);
+    // renderPokemonEvolutionsToBigCard(evoUrl, pokemonId);
 }
 
 
@@ -107,12 +133,95 @@ function closeBigPokemonCard() {
     CardRef.classList.remove(`pkm-card-overlay`);
     overflowContainer.classList.remove(`overflow-hidden`);
     CardRef.classList.add('d-none');
+}
 
+
+function filterPokemon() {
+    let searchInputRef = document.getElementById('search_input');
+    let searchInput = searchInputRef.value.toLowerCase();  // .toLowerCase() Konvertiert den Suchbegriff in Kleinbuschstaben 
+
+    if (searchInput.length >= 3) {
+        filtertPokemon = allPokemons.filter(pokemon => pokemon.name.toLowerCase().includes(searchInput));
+        renderFilteredPokemon();
+    } if (searchInput.length < 3) {
+        renderPokemonCard();
+    }
+}
+
+
+function renderFilteredPokemon() {
+    let pokemonCard = document.getElementById('pokemon_card');
+    pokemonCard.innerHTML = '';
+
+    for (let i = 0; i < filtertPokemon.length; i++) {
+        let firstType = filtertPokemon[i].type[0];
+        let weightInKg = (filtertPokemon[i].weight * 0.1).toFixed(1);
+
+        weightInKg = weightInKg.endsWith('.0') ? weightInKg.slice(0, -2) : weightInKg;
+        pokemonCard.innerHTML += getFilteredPokemonHTML(i, firstType, weightInKg);
+    }
+}
+
+
+function showNextBigCard(i) {
+    event.stopPropagation();
+    pokemon = (i + 1 + allPokemons.length) % allPokemons.length;
+
+    openBigPokemonCard(pokemon);
+}
+
+
+function showPrevBigCard(i) {
+    event.stopPropagation();
+    pokemon = (i - 1 + allPokemons.length) % allPokemons.length;
+
+    openBigPokemonCard(pokemon);
+}
+
+
+function showLoadingSpinner() {
+    let spinner = document.getElementById('loading_spinner');
+    let morePkm = document.getElementById('load_btn');
+
+    morePkm.classList.add('d-none');
+    spinner.classList.remove('d-none');
+}
+
+
+function hiddeLoadingSpinner() {
+    let spinner = document.getElementById('loading_spinner');
+    let morePkm = document.getElementById('load_btn');
+
+    spinner.classList.add('d-none');
+    morePkm.classList.remove('d-none');
+}
+
+
+async function loadAndShowPkm() {
+    showLoadingSpinner();
+
+    await fetchPokemon();
+    hiddeLoadingSpinner();
     renderPokemonCard();
 }
 
 
+function openInfoContainer(weightInKg, pokemon) {
+    let infoCard = document.getElementById(`stats_container`);
+    event.stopPropagation();
 
+    infoCard.innerHTML =``;
+    infoCard.innerHTML = getInfoContainerHTML(weightInKg, pokemon);
+}
+
+
+function openStatContainer(pokemon) {
+    let statsCard = document.getElementById(`stats_container`);
+    event.stopPropagation();
+
+    statsCard.innerHTML = ``;
+    statsCard.innerHTML = getPokemonStatsHTML(pokemon);
+}
 
 
 
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,6 +1,109 @@
 function getBigPokemonCardHTML(pokemon, firstType, weightInKg) {
     return `
-    <div class="pkm-card bg-${firstType}">
+    <div class="d-flex-c-c gap32">
+        <img onclick="showPrevBigCard(${pokemon},event)" class="prev-pkm-btn" src="./assets/img/arrow-left.png" alt="">
+        <div class="d-flex-c-c gap32">
+            <div class="pkm-card b-${firstType}">
+                <div class="d-flex-bw-c margin-8">
+                    <p>#${allPokemons[pokemon].id}</p>
+                    <div class="bg-${firstType}"></div>
+                </div>
+                <div class="d-flex-clm-c">
+                    <div class="pkm-img-container d-flex-c-c">
+                        <img class="pkm-img" src="${allPokemons[pokemon].image}" alt="pokemon bild">
+                    </div>
+                    <div>
+                        <h2 class="txt-center">${allPokemons[pokemon].name}</h2>
+                        <div class="d-flex-c-c d-clm m-t-32 m-b-16">
+                            <div class="m-b-16">
+                                <button class="big-card-buttons" onclick="openInfoContainer(${weightInKg},${pokemon})">Info</button>
+                                <button class="big-card-buttons" onclick="openStatContainer(${pokemon})">Stats</button>
+                            </div>
+                            <div id="stats_container">
+                                <div class="d-flex gap32">
+                                    <div class="d-flex-clm-c">
+                                        <span>Height</span>
+                                        <span>${((allPokemons[pokemon].height) / 10)}m</span>
+                                    </div>
+                                    <div class="d-flex-clm-c">
+                                        <span>weight</span>
+                                        <span>${weightInKg}kg</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <h3 class="txt-center m-b-32">Type: <b>${allPokemons[pokemon].type.join(', ')}</b></h3>
+                </div>
+            </div>
+        <div>
+        <img onclick="showNextBigCard(${pokemon},event)" class="next-pkm-btn" src="./assets/img/arrow-right.png" alt="">
+    </div>
+    `;
+}
+
+
+function getInfoContainerHTML(weightInKg, pokemon) {
+    return `
+    <div class="d-flex gap32">
+        <div class="d-flex-clm-c">
+            <span>Height</span>
+            <span>${((allPokemons[pokemon].height) / 10)}m</span>
+        </div>
+        <div class="d-flex-clm-c">
+            <span>weight</span>
+            <span>${weightInKg}kg</span>
+        </div>
+    </div>
+`;
+}
+
+
+function getPokemonStatsHTML(pokemon) {
+    return `
+    <div>
+        <h3 class="margin-8">Stats</h3>
+        <table>
+            <tr class="d-clm margin-8 gap8">
+                <td>HP: ${allPokemons[pokemon].stats[0]}</td>
+                <td>Atk: ${allPokemons[pokemon].stats[1]}</td>
+                <td>Def: ${allPokemons[pokemon].stats[2]}</td>
+                <td>Sp-Atk: ${allPokemons[pokemon].stats[3]}</td>
+                <td>Sp-Def: ${allPokemons[pokemon].stats[4]}</td>
+                <td>Int: ${allPokemons[pokemon].stats[5]}</td>
+            <tr>
+        </table>
+    </div>
+    `;
+}
+
+
+// function getPokemonEvolutionsHTML(evoUrl, pokemonId) {
+//     return `
+//     <div>
+//         <h4 class="margin-8">Evolutions</h4>
+//         <div class="d-flex gap8">
+//             <div class="d-clm margin-8">
+//                 <img>
+//                 <span>${pokemonId}</span>
+//             </div>
+//             <div class="d-clm margin-8">
+//                 <img>
+//                 <span>${pokemonId}</span>
+//             </div>
+//             <div class="d-clm margin-8">
+//                 <img>
+//                 <span>${pokemonId}</span>
+//             </div>
+//         </div>
+//     </div>
+//     `;
+// }
+
+
+function getAllPokemonHTML(pokemon, firstType, weightInKg) {
+    return `
+    <div id="pokemon${pokemon}" onclick="openBigPokemonCard(${pokemon})" class="pkm-card b-${firstType}">
         <div class="d-flex-bw-c margin-8">
             <p>#${allPokemons[pokemon].id}</p>
             <div>Typ Color</div>
@@ -11,7 +114,7 @@ function getBigPokemonCardHTML(pokemon, firstType, weightInKg) {
             </div>
             <div>
                 <h2 class="txt-center">${allPokemons[pokemon].name}</h2>
-                <div class="d-flex-c-c m-t-32 m-b-32 gap">
+                <div class="d-flex-c-c m-t-32 m-b-32 gap32">
                     <div class="d-flex-clm-c">
                         <span>Height</span>
                         <span>${((allPokemons[pokemon].height) / 10)}m</span>
@@ -21,64 +124,38 @@ function getBigPokemonCardHTML(pokemon, firstType, weightInKg) {
                         <span>${weightInKg}kg</span>
                     </div>
                 </div>
-                <h3 class="txt-center">Type: <b>${allPokemons[pokemon].type.join(', ')}</b></h3>
+                <h3 class="txt-center m-b-16">Type: <b>${allPokemons[pokemon].type.join(', ')}</b></h3>
             </div>
         </div>
     </div>
-    <div class="d-flex-clm-st">
-        <div class="stats-container">
-            <h3>Evolutions</h3>
-        </div>
-        <div id="pokemon_stats" class="stats-container">
-            <h3>Stats</h3>
-        </div>
-    </div>
-    `;
+`;
 }
 
 
-function getPokemonStatsHTML(hp, attack, defense, spezial_attack, spezial_defense, speed) {
+function getFilteredPokemonHTML(i, firstType, weightInKg) {
     return `
-    <div>
-        <table>
-            <tr class="d-clm">
-                <td>HP: ${hp}</td>
-                <td>Attack: ${attack}</td>
-                <td>Defense: ${defense}</td>
-                <td>Spezial-Attack: ${spezial_attack}</td>
-                <td>Spezial-Defense: ${spezial_defense}</td>
-                <td>Speed: ${speed}</td>
-            <tr>
-        </table>
-    </div>
-    `;
-}
-
-
-function getAllPokemonHTML(i, firstType, weightInKg) {
-    return `
-    <div onclick="openBigPokemonCard(${i})" class="pkm-card bg-${firstType}">
+    <div onclick="openBigPokemonCard(${i})" class="pkm-card b-${firstType}">
         <div class="d-flex-bw-c margin-8">
-            <p>#${allPokemons[i].id}</p>
+            <p>#${filtertPokemon[i].id}</p>
             <div>Typ Color</div>
         </div>
         <div class="d-flex-clm-c m-t-32">
             <div class="pkm-img-container d-flex-c-c">
-                <img class="pkm-img" src="${allPokemons[i].image}" alt="pokemon bild">
+                <img class="pkm-img" src="${filtertPokemon[i].image}" alt="pokemon bild">
             </div>
             <div>
-                <h2 class="txt-center">${allPokemons[i].name}</h2>
-                <div class="d-flex-c-c m-t-32 m-b-32 gap">
+                <h2 class="txt-center">${filtertPokemon[i].name}</h2>
+                <div class="d-flex-c-c m-t-32 m-b-32 gap32">
                     <div class="d-flex-clm-c">
                         <span>Height</span>
-                        <span>${((allPokemons[i].height) / 10)}m</span>
+                        <span>${((filtertPokemon[i].height) / 10)}m</span>
                     </div>
                     <div class="d-flex-clm-c">
                         <span>weight</span>
                         <span>${weightInKg}kg</span>
                     </div>
                 </div>
-                <h3 class="txt-center">Type: <b>${allPokemons[i].type.join(', ')}</b></h3>
+                <h3 class="txt-center m-b-16">Type: <b>${filtertPokemon[i].type.join(', ')}</b></h3>
             </div>
         </div>
     </div>
